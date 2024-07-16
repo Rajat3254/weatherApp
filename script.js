dab4fb08e72b183e6b15d8b2dea56879262d75cf
Rajat Singh <126982984+Rajat3254@users.noreply.github.com>
2024-07-17 00:36:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const searchWeather=document.querySelector('.SearchW');
 const search_container=document.querySelector('.searchContainer');
 const grantWeather_container=document.querySelector('.grantLocation');
 const searchBtn=document.querySelector('.search_btn');
-const city=document.querySelector('.searchContent');
+let cityt=document.querySelector('.searchContent');
 const errorScreen=document.querySelector('.error');
 const seacrhedWetherInfo=document.querySelector('.weatherInfo'); 
 const loadingScreen=document.querySelector('.load');
 const grant=document.querySelector('.grantAccess'); 
 let currTab=myWeather;
 const API_key='99a1e39dd299493c26846eece901e570';
-let data;
-//getFromSessionStorage();
+// let data;
+getFromSessionStorage();
 function swapTab(userTab){
     if(currTab!==userTab){
         currTab.classList.remove('bg-slate-200');
@@ -24,11 +24,12 @@ function swapTab(userTab){
             search_container.classList.add('flex');
             grantWeather_container.classList.remove('flex');
             grantWeather_container.classList.add('hidden');
+            seacrhedWetherInfo.classList.remove('flex');
+            seacrhedWetherInfo.classList.add('hidden');
+            errorScreen.classList.add('hidden');
         }else{
             search_container.classList.remove('flex');
             search_container.classList.add('hidden');
-            // grantWeather_container.classList.add('flex');
-            // grantWeather_container.classList.remove('hidden');
             getFromSessionStorage();
         }
     }
@@ -40,15 +41,16 @@ function getFromSessionStorage(){
         console.log('taer');
         grantWeather_container.classList.remove('hidden');
         grantWeather_container.classList.add('flex');
-        seacrhedWetherInfo.classList.add('hidden');
-        seacrhedWetherInfo.classList.remove('flex'); 
+        search_container.classList.add('hidden');
+        search_container.classList.remove('flex'); 
     }else{
+        console.log('not');
         grantWeather_container.classList.remove('flex');
         grantWeather_container.classList.add('hidden');
         const place=JSON.parse(localCoordinates);
         fetchInfo(place);
-        seacrhedWetherInfo.classList.add('hidden');
-        seacrhedWetherInfo.classList.remove('flex'); 
+        search_container.classList.add('hidden');
+        search_container.classList.remove('flex'); 
     }
 
 }
@@ -60,17 +62,28 @@ searchWeather.addEventListener('click',()=>{
  });
  async function fetchInfo(place){
     let city=place;
+    seacrhedWetherInfo.classList.add('hidden');
+    seacrhedWetherInfo.classList.remove('flex');
     loadingScreen.classList.remove('hidden');
     loadingScreen.classList.add('flex');
     try{
-     const info=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`);
+     const info=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`);
      console.log('fetched');
-     data=await info.json();
+    let data=await info.json();
+    if (data.cod !== 200) { 
+        throw new Error('City not found');
+    }
+    grantWeather_container.classList.remove('flex');
+     grantWeather_container.classList.add('hidden');
      loadingScreen.classList.add('hidden');
      loadingScreen.classList.remove('flex');
+    
      renderInfo(data);
+     seacrhedWetherInfo.classList.add('flex');
+     seacrhedWetherInfo.classList.remove('hidden');
     }catch{
-
+        errorScreen.classList.remove('hidden');
+         loadingScreen.classList.add('hidden');
     }
  }
  async function renderInfo(data){
@@ -78,7 +91,7 @@ searchWeather.addEventListener('click',()=>{
     let flag=document.querySelector('.flag_img');
     let weatherCondition=document.querySelector('.Weather_condition');
     let conditionImg=document.querySelector('.Condition_img');
-    let temperature=document.querySelector('.tempreture');
+    let temperature=document.querySelector('.temperature');
     let windSpeed=document.querySelector('.speed');
     let humidity=document.querySelector('.humidity');
     let clouds=document.querySelector('.cloud');
@@ -86,26 +99,63 @@ searchWeather.addEventListener('click',()=>{
     flag.src=`https://flagcdn.com/144x108/${data?.sys?.country?.toLowerCase()}.png`;
     weatherCondition.innerText=data?.weather?.[0]?.description;
     conditionImg.src=`http://openweathermap.org/img/w/${data?.weather?.[0]?.icon}.png`;
-    temperature.innerText=data?.main?.temp;
-    windSpeed.innerText=data?.wind?.speed;
-    humidity.innerText=data?.main?.humidity;
-    clouds.innerText=data?.clouds?.all;
+    temperature.innerText= `${data?.main?.temp} °C`;
+    windSpeed.innerText=`${data?.wind?.speed} km/s`;
+    humidity.innerText=`${data?.main?.humidity}%`;
+    clouds.innerText=`${data?.clouds?.all}%`;
+    
 
  }
  searchBtn.addEventListener('click',()=>{
-    
+    const cityTofindWeather=cityt.value;
+    console.log(cityTofindWeather);
+    if(!cityTofindWeather){
+        errorScreen.classList.remove('hidden');
+        seacrhedWetherInfo.classList.add('hidden');
+        seacrhedWetherInfo.classList.remove('flex');
+    }else{
+        errorScreen.classList.add('hidden');
+        fetchInfo(cityTofindWeather);
+    }
  });
+
+ cityt.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        if (!cityt.value.trim()) {
+            errorScreen.classList.remove('hidden');
+        } else {
+            errorScreen.classList.add('hidden');
+            fetchInfo(cityt.value.trim());
+        }
+    }
+});
+
  grant.addEventListener('click',()=>{
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showplace);
+        navigator.geolocation.getCurrentPosition(showPlace);
     }else{
         
     }
 
  });
- 
- function showplace(position){
-    let city=?;
-    sessionStorage.setItem("user",JSON.stringify(city));
-    fetchInfo(city);
- }
+
+function showPlace(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    
+    // Use a reverse geocoding API to convert coordinates to a city name
+    fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}&localityLanguage=en`)
+        .then(response => response.json())
+        .then(data => {
+            const city = data.city;
+            if (city) {
+                sessionStorage.setItem("user", JSON.stringify(city));
+                fetchInfo(city);
+            } else {
+                console.error('City not found in geolocation data');
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching geolocation data:', error);
+        });
+}
